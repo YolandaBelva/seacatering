2dac980f43397719919305429dc9f054b3c5b96d
Yolanda Belva <belvayolanda22@gmail.com>
2025-06-25 10:16:03 +0700

    add migration subscription

---
+++ database/migrations/2025_06_23_042108_create_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('phone_number');
+            $table->foreignId('plan_id')->constrained('meal_plans')->onDelete('cascade');
+            $table->string('mealType');
+            $table->string('delivery_days');
+            $table->text('allergies')->nullable();
+            $table->enum('status', ['ACTIVE', 'PAUSE', 'CANCEL']);
+            $table->date('end_date')->nullable();
+            $table->date('pause_period_start')->nullable();
+            $table->date('pause_period_end')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
