3f9c3dee0087492ed56ec4d4a98ffbe2edef77e9
Yolanda Belva <belvayolanda22@gmail.com>
2025-06-28 13:19:47 +0700

    update model testiminial

--- app/Models/Testimonial.php
+++ app/Models/Testimonial.php
@@ -2,16 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Testimonial extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'user_id',
-        'review',
-        'rating',
-    ];
+    protected $fillable = ['name', 'rating', 'review'];
 }
