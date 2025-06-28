c934065533b2bbde6e1c0a7971f20bc72bc490f0
Yolanda Belva <belvayolanda22@gmail.com>
2025-06-28 13:19:30 +0700

    update model subscription

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,17 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
-        'phone_number',
+        'phone',
         'plan_id',
-        'mealType',
+        'meal_types',
         'delivery_days',
         'allergies',
         'status',
+        'pause_from',
+        'pause_until',
     ];
+
+
+    protected $casts = [
+        'meal_types' => 'array',
+        'delivery_days' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(MealPlan::class, 'plan_id');
+    }
 }
