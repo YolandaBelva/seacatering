eae9ed9f0cfd6047b4a9b4234e4551569a6a89d9
Yolanda Belva <belvayolanda22@gmail.com>
2025-07-01 15:58:57 +0700

    update password admin

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
@@ -10,14 +8,21 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
-            ['email' => 'admin@seacatering.com'],
-            [
+        $admin = User::where('email', 'admin@seacatering.com')->first();
+
+        if ($admin) {
+            // Force update password
+            $admin->update([
+                'password' => Hash::make('Adminsea@123')
+            ]);
+        } else {
+            // Create new admin
+            User::create([
                 'fullname' => 'admin',
                 'email' => 'admin@seacatering.com',
-                'password' => Hash::make('admin123'),
+                'password' => Hash::make('Adminsea@123'),
                 'role' => 'admin'
-            ]
-        );
+            ]);
+        }
     }
 }
